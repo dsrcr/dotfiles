af0a9e5be20bdc2fcd3e5b9c57fa84938fb3b754
dsorcererdev <dsorcererdev@yahoo.com>
2023-01-09 21:06:06 +0100

    Separate neovim config from dotfiles

--- nvim/lua/language/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "language.lsp.lsp-installer"
-require("language.lsp.handlers").setup()
-require "language.lsp.null-ls"
