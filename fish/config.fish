e69697b0f517fa6680452b5a6cc13a71226b1596
dsrcr <dscr@duck.com>
2023-02-21 10:29:58 +0000

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,14 @@
-# ________     _________                        _____ .__    ____   
-# \______ \   /   _____/  ____   ____    ____ _/ ____\|__|  / ___\  
-#  |    |  \  \_____  \ _/ ___\ /  _ \  /    \\   __\ |  | / /_/  > 
-#  |    `   \ /        \\  \___(  <_> )|   |  \|  |   |  | \___  /  
-# /_______  //_______  / \___  >\____/ |___|  /|__|   |__|/_____/   
-#         \/         \/      \/             \/                      
-# Custom minimalistic fish(starship) config
-# Trying to keep it simple, as always...
-# https://github.com/dsorcererdev/dotfiles  
-
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
     end
 end
+# Startx end
 
 # starship prompt startup
 starship init fish | source
-
+# startship end
 
 # pnpm
 set -gx PNPM_HOME "/home/ds/.local/share/pnpm"
